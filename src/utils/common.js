2ba5aafb57f96b835569b2bcba283c717ed77200
Gilbok Lee <gilbok@live.com>
2021-03-12 00:56:03 +0700

    user more user-friendly hourCycle for Tested at message

--- src/utils/common.js
+++ src/utils/common.js
@@ -25,7 +25,7 @@ function checkEnvVar(variable) {
 }
 
 function generateCurrentTime() {
-    return new Date().toLocaleTimeString([], {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hourCycle: 'h24'});
+    return new Date().toLocaleTimeString([], {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hourCycle: 'h23'});
 }
 
 function generateTimestamp() {
