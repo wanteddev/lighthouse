63b00a4d2cc17430626e63dd9445a317ac788cb8
Andre Vasconcelos <andre.onogoro@gmail.com>
2021-03-23 11:59:07 +0900

    WIP: Re-working throttling to be more reliable
    - Making it so that disabling throttling both affects CPU and network throttling

--- src/utils/lighthouse.js
+++ src/utils/lighthouse.js
@@ -41,26 +41,49 @@ async function launchPuppeteer(url, options) {
 
         // Lighthouse will open URL. Puppeteer observes `targetchanged` and sets up network conditions.
         // Possible race condition.
-        let opts = {
+        let flags = {
             port: (new URL(browser.wsEndpoint())).port,
             output: 'json',
             logLevel: 'error',
-            onlyCategories: [],
-            screenEmulation: {
-                disabled: true,
-            },
         };
 
-        if (options.performance) opts.onlyCategories.push('performance');
-        if (options.accessibility) opts.onlyCategories.push('accessibility');
-        if (options['best-practices']) opts.onlyCategories.push('best-practices');
-        if (options.pwa) opts.onlyCategories.push('pwa');
-        if (options.seo) opts.onlyCategories.push('seo');
+        let opts = {
+            settings: {
+                onlyCategories: [],
+                screenEmulation: {
+                    disabled: true,
+                },
+            }
+        }
+
+        if (options.performance) opts.settings.onlyCategories.push('performance');
+        if (options.accessibility) opts.settings.onlyCategories.push('accessibility');
+        if (options['best-practices']) opts.settings.onlyCategories.push('best-practices');
+        if (options.pwa) opts.settings.onlyCategories.push('pwa');
+        if (options.seo) opts.settings.onlyCategories.push('seo');
         
         // as throttling is enabled by default in lighthouse, disable it if explicitly unchecked
-        if (options.throttling === false) opts.throttlingMethod = 'provided';
+        if (options.throttling === false) {
+            opts.settings.throttlingMethod = 'provided';
+            opts.settings.emulatedUserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4420.0 Safari/537.36 Chrome-Lighthouse';
+            opts.settings.throttling = {
+                rttMs: 40,
+                throughputKbps: 10 * 1024,
+                cpuSlowdownMultiplier: 1,
+                requestLatencyMs: 0,
+                downloadThroughputKbps: 0,
+                uploadThroughputKbps: 0,
+            };
+            opts.settings.screenEmulation = {
+                mobile: false,
+                width: 1350,
+                height: 940,
+                deviceScaleFactor: 1,
+                disabled: false,
+            }
+        }
 
-        const {lhr} = await lighthouse(url, opts);
+        const {lhr} = await lighthouse(url, flags, opts);
         // Return response back to main thread
         parentPort.postMessage(lhr);
 
